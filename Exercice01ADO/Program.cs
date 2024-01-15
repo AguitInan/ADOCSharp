ce1147474cec063a262e1587c3e22df04fb63096
unknown <aguit.inan@gmail.com>
2024-01-15 16:31:37 +0100

    feat: add SQLQuery1.sql Exercice01ADO

--- Exercice01ADO/Program.cs
+++ Exercice01ADO/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Core;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+// connectionString
+string connectionString = "Data Source=(localdb)\\demo01ado; Integrated Security=True; Database=Contact";
+
+//// Création d'un objet SQLConnection présent dans la librairie ADO.NET
+SqlConnection connection = new SqlConnection(connectionString);
+
+// Lire des enregistrements (Afficher la totalité des étudiants)
+using (SqlConnection conn = new SqlConnection(connectionString))
+{
+    string req = "SELECT * FROM etudiant;";
+
+    SqlCommand command = new SqlCommand(req, conn);
+
+    try
+    {
+        conn.Open();
+
+        // Lorsque l'on souhaite lire des enregistrements, on instancie un reader
+        SqlDataReader reader = command.ExecuteReader();
+
+        // La méthode Read renvoie vraie tant qu'il y a des enregistrements
+        while (reader.Read())
+        {
+            // On récupère la valeur d'un enregistrement grâce à son index
+            Console.WriteLine($"id: {reader.GetInt32(0)}, prenom: {reader.GetString(1)}, nom: {reader.GetString(2)}, email: {reader.GetString(3)}");
+        }
+
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Etudiant] (
+    [id]     INT           IDENTITY (1, 1) NOT NULL,
+    [nom] VARCHAR (50)  NOT NULL,
+    [prenom]    VARCHAR (50)  NOT NULL,
+    [numeroClasse]  VARCHAR (255) NOT NULL,
+    [dateDiplome]  DATETIME  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
