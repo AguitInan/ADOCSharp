bec36c8bdefa9c67680c4ebe2d3cb6fd8e0bf4ef
unknown <aguit.inan@gmail.com>
2024-01-15 16:21:57 +0100

    feat: add Exercice01ADO

--- Demo01Ado/Program.cs
+++ Demo01Ado/Program.cs
@@ -5,38 +5,42 @@ using System.Data;
 
 string connectionString = "Data Source=(localdb)\\demo01ado; Integrated Security=True ; Database=Contact";
 
-SqlConnection connection = new SqlConnection(connectionString);
+//SqlConnection connection = new SqlConnection(connectionString);
 
 
-connection.Open();
+//connection.Open();
 
-if (connection.State == ConnectionState.Open)
+//if (connection.State == ConnectionState.Open)
+//{
+//    Console.WriteLine("La connexion est ouverte!");
+//}
+//else
+//{
+//    Console.WriteLine("La connexion n'est pas ouverte :(");
+//}
+
+//// Requête
+
+//Console.WriteLine("Prenom");
+//string prenom = Console.ReadLine() ?? "";
+//Console.WriteLine("Nom");
+//string nom = Console.ReadLine() ?? "";
+//Console.WriteLine("Email");
+//string email = Console.ReadLine() ?? "";
+
+//string req = $"INSERT INTO personne (prenom, nom, email) VALUES ('{prenom}','{nom}','{email}')";
+
+//SqlCommand command = new SqlCommand(req, connection);
+
+//int rowsAffected = command.ExecuteNonQuery();
+
+//Console.WriteLine(rowsAffected);
+
+//command.Dispose();
+
+//connection.Close();
+
+using (SqlConnection conn = new SqlConnection(connectionString))
 {
-    Console.WriteLine("La connexion est ouverte!");
+    string request = "INSERT INTO personne (prenom, nom, email) VALUES (@prenom, @nom, @email)";
 }
-else
-{
-    Console.WriteLine("La connexion n'est pas ouverte :(");
-}
-
-// Requête
-
-Console.WriteLine("Prenom");
-string prenom = Console.ReadLine() ?? "";
-Console.WriteLine("Nom");
-string nom = Console.ReadLine() ?? "";
-Console.WriteLine("Email");
-string email = Console.ReadLine() ?? "";
-
-string req = $"INSERT INTO personne (prenom, nom, email) VALUES ('{prenom}','{nom}','{email}')";
-
-SqlCommand command = new SqlCommand(req, connection);
-
-int rowsAffected = command.ExecuteNonQuery();
-
-Console.WriteLine(rowsAffected);
-
-command.Dispose();
-
-connection.Close();
-
---
+++ Exercice01ADO/Classes/Etudiant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice01ADO.Classes
+{
+    internal class Etudiant
+    {
+        private int _id;
+        private string _nom;
+        private string _prenom;
+        private string _numeroClasse;
+        private DateTime _dateDiplome;
+
+
+        public int Id { get => _id; set => _id = value; }
+        public string Nom { get => _nom; set => _nom = value; }
+        public string Prenom { get => _prenom; set => _prenom = value; }
+        public string NumeroClasse { get => _numeroClasse; set => _numeroClasse = value; }
+        public DateTime DateDiplome { get => _dateDiplome; set => _dateDiplome = value; }
+
+
+
+        public Etudiant()
+        {
+
+        }
+        public Etudiant(string n, string p, string c, DateTime d)
+        {
+            Nom = n;
+            Prenom = p;
+            NumeroClasse = c;
+            DateDiplome = d;
+        }
+
+   
+    }
+}
