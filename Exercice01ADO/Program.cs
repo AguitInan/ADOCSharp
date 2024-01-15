5a8e3c7b7a439ddfdcf3b8fd86013e0e24d5e95e
unknown <aguit.inan@gmail.com>
2024-01-15 16:45:22 +0100

    feat: update Program.cs Exercice01ADO

--- Exercice01ADO/Program.cs
+++ Exercice01ADO/Program.cs
@@ -26,7 +26,44 @@ using (SqlConnection conn = new SqlConnection(connectionString))
         while (reader.Read())
         {
             // On récupère la valeur d'un enregistrement grâce à son index
-            Console.WriteLine($"id: {reader.GetInt32(0)}, prenom: {reader.GetString(1)}, nom: {reader.GetString(2)}, email: {reader.GetString(3)}");
+            Console.WriteLine($"id: {reader.GetInt32(0)}, prenom: {reader.GetString(1)}, nom: {reader.GetString(2)}, numeroClasse: {reader.GetString(3)}, dateDiplome: {reader.GetString(3)}");
+        }
+
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+
+}
+
+// Lire des enregistrements (Afficher les étudiants d'une classe)
+using (SqlConnection conn = new SqlConnection(connectionString))
+{
+    string req = "SELECT * FROM etudiant WHERE numeroClasse=@numeroClasse;";
+
+    int numeroClasse = int.Parse(Console.ReadLine());
+
+    SqlCommand command = new SqlCommand(req, conn);
+
+    command.Parameters.AddWithValue("@numeroClasse", numeroClasse);
+
+    try
+    {
+        conn.Open();
+
+        // Lorsque l'on souhaite lire des enregistrements, on instancie un reader
+        SqlDataReader reader = command.ExecuteReader();
+
+        // La méthode Read renvoie vraie tant qu'il y a des enregistrements
+        if (reader.Read())
+        {
+            // On récupère la valeur d'un enregistrement grâce à son index
+            Console.WriteLine($"id: {reader.GetInt32(0)}, prenom: {reader.GetString(1)}, nom: {reader.GetString(2)}, numeroClasse: {reader.GetString(3)}, dateDiplome: {reader.GetString(3)}");
+        }
+        else
+        {
+            Console.WriteLine($"Aucun étudiant n'a été trouvé dans la classe {numeroClasse}");
         }
 
     }
