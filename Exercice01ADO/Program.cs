4615f94410c3d78e7c3e26ff6ab6b31f28ed19bc
unknown <aguit.inan@gmail.com>
2024-01-16 11:31:19 +0100

    feat: update Program.cs Exercice01ADO

--- Exercice01ADO/Program.cs
+++ Exercice01ADO/Program.cs
@@ -1,75 +1,19 @@
-﻿using Azure.Core;
+﻿using System;
+using Azure.Core;
+using Exercice01ADO.Classes;
 using Microsoft.Data.SqlClient;
 using System.Data;
 
-// connectionString
-string connectionString = "Data Source=(localdb)\\demo01ado; Integrated Security=True; Database=Contact";
 
-//// Création d'un objet SQLConnection présent dans la librairie ADO.NET
-SqlConnection connection = new SqlConnection(connectionString);
-
-// Lire des enregistrements (Afficher la totalité des étudiants)
-using (SqlConnection conn = new SqlConnection(connectionString))
+namespace Exercice01ADO
 {
-    string req = "SELECT * FROM etudiant;";
-
-    SqlCommand command = new SqlCommand(req, conn);
-
-    try
+    class Program
     {
-        conn.Open();
-
-        // Lorsque l'on souhaite lire des enregistrements, on instancie un reader
-        SqlDataReader reader = command.ExecuteReader();
-
-        // La méthode Read renvoie vraie tant qu'il y a des enregistrements
-        while (reader.Read())
+        static void Main(string[] args)
         {
-            // On récupère la valeur d'un enregistrement grâce à son index
-            Console.WriteLine($"id: {reader.GetInt32(0)}, prenom: {reader.GetString(1)}, nom: {reader.GetString(2)}, numeroClasse: {reader.GetString(3)}, dateDiplome: {reader.GetString(3)}");
+            IHM.Menu();
         }
-
     }
-    catch (Exception e)
-    {
-        Console.WriteLine(e.Message);
-    }
-
 }
 
-// Lire des enregistrements (Afficher les étudiants d'une classe)
-using (SqlConnection conn = new SqlConnection(connectionString))
-{
-    string req = "SELECT * FROM etudiant WHERE numeroClasse=@numeroClasse;";
 
-    int numeroClasse = int.Parse(Console.ReadLine());
-
-    SqlCommand command = new SqlCommand(req, conn);
-
-    command.Parameters.AddWithValue("@numeroClasse", numeroClasse);
-
-    try
-    {
-        conn.Open();
-
-        // Lorsque l'on souhaite lire des enregistrements, on instancie un reader
-        SqlDataReader reader = command.ExecuteReader();
-
-        // La méthode Read renvoie vraie tant qu'il y a des enregistrements
-        if (reader.Read())
-        {
-            // On récupère la valeur d'un enregistrement grâce à son index
-            Console.WriteLine($"id: {reader.GetInt32(0)}, prenom: {reader.GetString(1)}, nom: {reader.GetString(2)}, numeroClasse: {reader.GetString(3)}, dateDiplome: {reader.GetString(3)}");
-        }
-        else
-        {
-            Console.WriteLine($"Aucun étudiant n'a été trouvé dans la classe {numeroClasse}");
-        }
-
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e.Message);
-    }
-
-}
