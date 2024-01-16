7899ce514de1ddd396df0a09dfe065bafa6f530e
unknown <aguit.inan@gmail.com>
2024-01-16 11:05:17 +0100

    feat: update Etudiant.cs Exercice01ADO

--- Exercice01ADO/Classes/Etudiant.cs
+++ Exercice01ADO/Classes/Etudiant.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using Exercice01ADO.Data;
 
 namespace Exercice01ADO.Classes
 {
@@ -13,14 +14,14 @@ namespace Exercice01ADO.Classes
         private string _nom;
         private string _prenom;
         private string _numeroClasse;
-        private DateTime _dateDiplome;
+        private string _dateDiplome;
 
 
         public int Id { get => _id; set => _id = value; }
         public string Nom { get => _nom; set => _nom = value; }
         public string Prenom { get => _prenom; set => _prenom = value; }
         public string NumeroClasse { get => _numeroClasse; set => _numeroClasse = value; }
-        public DateTime DateDiplome { get => _dateDiplome; set => _dateDiplome = value; }
+        public string DateDiplome { get => _dateDiplome; set => _dateDiplome = value; }
 
 
 
@@ -28,14 +29,27 @@ namespace Exercice01ADO.Classes
         {
 
         }
-        public Etudiant(string n, string p, string c, DateTime d)
+        public Etudiant(string n, string p, string c, string d)
         {
+            //Id = id;
             Nom = n;
             Prenom = p;
             NumeroClasse = c;
             DateDiplome = d;
         }
 
-   
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+
