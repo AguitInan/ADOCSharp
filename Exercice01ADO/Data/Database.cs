80eea61778b1619b1550f915ecad314c069d876f
unknown <aguit.inan@gmail.com>
2024-01-16 11:33:21 +0100

    feat: add Database.cs Exercice01ADO

---
+++ Exercice01ADO/Data/Database.cs
@@ -0,0 +1,26 @@
+﻿using Exercice01ADO.Classes;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exercice01ADO.Data
+{
+    internal class DataBase
+    {
+        private static string chaine = "Data Source=(localdb)\\demo01ado; Integrated Security=True; Database=Contact";
+        public static SqlConnection Connection = new SqlConnection(chaine);
+
+
+
+
+
+
+
+
+
+    }
+}
