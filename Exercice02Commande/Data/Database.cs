f6b6566ac3c23b5e312146ee799cfca8709b4d0a
unknown <aguit.inan@gmail.com>
2024-01-17 14:34:39 +0100

    feat: update Database.cs Exercice02Commande

--- Exercice02Commande/Data/Database.cs
+++ Exercice02Commande/Data/Database.cs
@@ -15,7 +15,35 @@ namespace Exercice02Commande.Data
         private static string chaine = "Data Source=(localdb)\\demo01ado; Integrated Security=True; Database=Commande";
         public static SqlConnection Connection = new SqlConnection(chaine);
 
-        
+        // Méthodes pour la gestion des Clients
+        public static bool AjouterClient(Client client)
+        {
+            try
+            {
+                using (SqlCommand command = new SqlCommand("INSERT INTO client (Nom, Prenom, Adresse, CodePostal, Ville, Telephone) VALUES (@Nom, @Prenom, @Adresse, @CodePostal, @Ville, @Telephone)", Connection))
+                {
+                    command.Parameters.AddWithValue("@Nom", client.Nom);
+                    command.Parameters.AddWithValue("@Prenom", client.Prenom);
+                    command.Parameters.AddWithValue("@Adresse", client.Adresse);
+                    command.Parameters.AddWithValue("@CodePostal", client.CodePostal);
+                    command.Parameters.AddWithValue("@Ville", client.Ville);
+                    command.Parameters.AddWithValue("@Telephone", client.Telephone);
+
+                    Connection.Open();
+                    int result = command.ExecuteNonQuery();
+                    return result > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
 
         
 
