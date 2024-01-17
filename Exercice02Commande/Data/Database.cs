4fbf9618366e19863e5cb051befacabe91c715a0
unknown <aguit.inan@gmail.com>
2024-01-17 14:34:12 +0100

    feat: add Database.cs Exercice02Commande

---
+++ Exercice02Commande/Data/Database.cs
@@ -0,0 +1,34 @@
+﻿using Exercice02Commande.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+using System.Data.SqlClient;
+
+namespace Exercice02Commande.Data
+{
+    internal class DataBase
+    {
+        private static string chaine = "Data Source=(localdb)\\demo01ado; Integrated Security=True; Database=Commande";
+        public static SqlConnection Connection = new SqlConnection(chaine);
+
+        
+
+        
+
+        
+
+        
+
+        
+
+        
+
+        
+
+        
+    }
+}
