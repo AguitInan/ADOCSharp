eeef5358c2b4fc9b8da044e80a26c3152ed8ecbe
unknown <aguit.inan@gmail.com>
2024-01-17 14:35:17 +0100

    feat: update Database.cs Exercice02Commande

--- Exercice02Commande/Data/Database.cs
+++ Exercice02Commande/Data/Database.cs
@@ -69,7 +69,35 @@ namespace Exercice02Commande.Data
             }
         }
 
-        
+        public static bool ModifierClient(Client client)
+        {
+            try
+            {
+                using (SqlCommand command = new SqlCommand("UPDATE client SET Nom = @Nom, Prenom = @Prenom, Adresse = @Adresse, CodePostal = @CodePostal, Ville = @Ville, Telephone = @Telephone WHERE Id = @Id", Connection))
+                {
+                    command.Parameters.AddWithValue("@Id", client.Id);
+                    command.Parameters.AddWithValue("@Nom", client.Nom);
+                    command.Parameters.AddWithValue("@Prenom", client.Prenom);
+                    command.Parameters.AddWithValue("@Adresse", client.Adresse);
+                    command.Parameters.AddWithValue("@CodePostal", client.CodePostal);
+                    command.Parameters.AddWithValue("@Ville", client.Ville);
+                    command.Parameters.AddWithValue("@Telephone", client.Telephone);
+
+                    Connection.Open();
+                    int result = command.ExecuteNonQuery();
+                    return result > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
 
         
 
