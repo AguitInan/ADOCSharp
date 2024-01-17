7e9aa0d6008b239b2e9e2e8185d07af7f2ce1689
unknown <aguit.inan@gmail.com>
2024-01-17 14:23:19 +0100

    feat: add Commande.cs Exercice02Commande

---
+++ Exercice02Commande/Classes/Commande.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice02Commande.Classes
+{
+    internal class Commande
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Total { get; set; }
+
+
+
+
+    }
+}
