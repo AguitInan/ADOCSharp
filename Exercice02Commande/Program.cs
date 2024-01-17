4ec5547fdf6d0d1ecf62118c61ac3cc3f68f9896
unknown <aguit.inan@gmail.com>
2024-01-17 14:19:30 +0100

    feat: add Program.cs Exercice02Commande

---
+++ Exercice02Commande/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Azure.Core;
+using Exercice02Commande.Classes;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace Exercice02Commande
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHM.Menu();
+        }
+    }
+}
+
+
